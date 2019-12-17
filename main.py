12fdde9714862a956c2d05cf1bd07ffe9a993519
dungpham98 <phamtuandung12@gmail.com>
2019-12-17 08:39:16 +0700

    remove while loop

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ import regression_model as rm
 
 fileName = 'Variables.csv'
 cooks_d = rm.model(fileName)
-while(len(cooks_d) > 0):
-	print(cooks_d)
-	ds = pd.read_csv(fileName)
-	for(index,value) in cooks_d:
-		ds = ds.drop([index],axis = 0)
-	idx = 0
-	fileName = 'Variables'+str(idx)+'.csv'
-	ds.to_csv(fileName, index=False, encoding='utf8')
-	cooks_d = rm.model(fileName)
-	idx = idx + 1
+print(cooks_d)
+ds = pd.read_csv(fileName)
+for(index,value) in cooks_d:
+	ds = ds.drop([index],axis = 0)
+idx = 0
+fileName = 'Variables'+str(idx)+'.csv'
+ds.to_csv(fileName, index=False, encoding='utf8')
+cooks_d = rm.model(fileName)
